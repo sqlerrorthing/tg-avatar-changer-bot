f1612c92e3681bd2f45bed05c4773a92af4073c6
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-10-14 09:49:12 +0800

    error handlers

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use log::{error, info, warn};
+use regex::Regex;
 use std::time::Duration;
 
 use derive_new::new;
@@ -6,7 +8,7 @@ use tokio::time::sleep;
 use tdlib_rs::{
     enums::{InputChatPhoto, InputFile},
     functions,
-    types::{InputChatPhotoStatic, InputFileLocal},
+    types::{Error, InputChatPhotoStatic, InputFileLocal},
 };
 
 use crate::avatar_api::AvatarProvider;
@@ -24,7 +26,7 @@ impl<P: AvatarProvider> AvatarChanger<P> {
     pub async fn run_loop(&self) {
         loop {
             let avatar = self.provider.fetch_avatar().await.unwrap();
-            println!("Setting avatar: {}x{}", avatar.width(), avatar.height());
+            info!("Setting avatar: {}x{}", avatar.width(), avatar.height());
 
             let file = tempfile::NamedTempFile::new().unwrap();
             avatar
@@ -35,22 +37,58 @@ impl<P: AvatarProvider> AvatarChanger<P> {
                 path: file.path().display().to_string(),
             };
 
-            functions::set_profile_photo(
-                InputChatPhoto::Static(InputChatPhotoStatic {
-                    photo: InputFile::Local(local),
-                }),
-                false,
-                self.client_id,
-            )
-            .await
-            .unwrap();
+            if let Err(err) = try_set_profile_photo(InputFile::Local(local), self.client_id).await {
+                error!("Failed to set profile photo: {err:?}");
+            } else {
+                info!("Avatar has been set");
+            }
 
-            println!(
-                "Avatar has been set, waiting {:.2} seconds",
-                self.change_duration.as_secs_f32()
-            );
+            info!("Waiting {:.2} seconds", self.change_duration.as_secs_f32());
 
             sleep(self.change_duration).await;
         }
+    }
+}
+
+async fn try_set_profile_photo(photo: InputFile, client_id: i32) -> Result<(), Error> {
+    loop {
+        let Err(err) = functions::set_profile_photo(
+            InputChatPhoto::Static(InputChatPhotoStatic {
+                photo: photo.clone(),
+            }),
+            false,
+            client_id,
+        )
+        .await
+        else {
+            return Ok(());
+        };
+
+        match err.code {
+            429 => {
+                let retry_after = parse_retry_time(&err.message).ok_or(err)? + 1;
+                warn!("Timeouted, retrying after {retry_after}s");
+                sleep(Duration::from_secs(retry_after)).await;
+            }
+            _ => return Err(err),
+        }
+    }
+}
+
+fn parse_retry_time(text: &str) -> Option<u64> {
+    let re = unsafe { Regex::new(r"retry after (\d+)").unwrap_unchecked() };
+
+    re.captures(text)
+        .and_then(|caps| caps.get(1)?.as_str().parse::<u64>().ok())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_retry_time;
+
+    #[test]
+    fn prase_retry_time() {
+        let message = "Too Many Requests: retry after 294";
+        assert_eq!(parse_retry_time(message), Some(294))
     }
 }
