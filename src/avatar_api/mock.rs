05059501015db5c68c10c1ddf685de04d538dcf2
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2025-10-14 12:05:48 +0800

    changes...

--- src/avatar_api/mock.rs
+++ src/avatar_api/mock.rs
@@ -1,8 +1,9 @@
-use std::future::{Ready, ready};
+use std::time::Duration;
 
 use derive_new::new;
 use image::{DynamicImage, Rgba, RgbaImage};
 use rand::Rng;
+use tokio::time::sleep;
 
 use crate::avatar_api::{AvatarProvider, FetchError};
 
@@ -16,13 +17,15 @@ impl Default for MockAvatarProvider {
 }
 
 impl AvatarProvider for MockAvatarProvider {
-    type FetchAvatarFuture<'a> = Ready<Result<DynamicImage, FetchError>>;
-
-    fn fetch_avatar<'a>(&'a self) -> Self::FetchAvatarFuture<'a> {
-        let mut rng = rand::rng();
-        let color = Rgba([rng.random(), rng.random(), rng.random(), 255]);
+    async fn fetch_avatar<'a>(&'a self) -> Result<DynamicImage, FetchError> {
+        let color = {
+            let mut rng = rand::rng();
+            Rgba([rng.random(), rng.random(), rng.random(), 255])
+        };
 
         let img = RgbaImage::from_pixel(self.0, self.0, color);
-        ready(Ok(DynamicImage::ImageRgba8(img)))
+        sleep(Duration::from_secs(2)).await;
+
+        Ok(DynamicImage::ImageRgba8(img))
     }
 }
